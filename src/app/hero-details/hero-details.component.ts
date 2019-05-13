37484ea2a7eef7385aaa4fbfea6bb135beb24c2f
Alek Hristov <alek.hristov@resolutesoftware.com>
2019-05-13 13:16:46 +0300

    Finalized Angular Heroes Tutorial

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -39,4 +39,9 @@ export class HeroDetailsComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
